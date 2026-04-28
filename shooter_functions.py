38a09c595263f085ac5813c697cb75adaae32128
Joel Sasot <83161297+joelsasot@users.noreply.github.com>
2026-04-28 21:07:09 +0300

    Update shooter_functions.py

--- shooter_functions.py
+++ shooter_functions.py
@@ -74,6 +74,7 @@ def create_shooter_dataset(team_data,criteria):
     
     for player in top_players['Name']:
         st.write(top_players[top_players['Name']==player])
+        st.write(top_players[top_players['Name']==player]['shooting_team'].unique())
         
         if len(top_players[top_players['Name']==player]['shooting_team'].unique())>1:
             # Then that player has been a top shooter for more than one team. Her data should not be shown together
